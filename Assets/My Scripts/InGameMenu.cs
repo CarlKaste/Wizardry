dcf64b4f02d39e22622e1df8cea69a7e356752e7
CarlKaste <117178619+CarlKaste@users.noreply.github.com>
2023-03-29 17:58:42 +0200

    No change (test)

--- Assets/My Scripts/InGameMenu.cs
+++ Assets/My Scripts/InGameMenu.cs
@@ -7,6 +7,11 @@ public class InGameMenu : MonoBehaviour
 {
     private bool isActive = false;
 
+    private void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
+
     public void ActivateMenu()
     {
         if (isActive == false)
